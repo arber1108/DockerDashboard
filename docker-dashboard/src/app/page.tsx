e446721998343e37b932bc70c5714e992bef70ff
ArberAdemaj <arber.ademaj@students.fhnw.ch>
2026-01-12 14:53:00 +0100

    install shadcn and add card component

--- docker-dashboard/src/app/page.tsx
+++ docker-dashboard/src/app/page.tsx
@@ -1,7 +1,29 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 export default function Home() {
   return (
     <div>
       <h1 className="flex justify-center pt-5 text-6xl">Docker Dashboard</h1>
+      <div className="flex flex-row gap-3 pt-5 m-5">
+        <div className="w-xl">
+          <Card>
+            <CardHeader>
+              <CardTitle>Container Titel</CardTitle>
+              <CardDescription>Image</CardDescription>
+              <CardAction>Manage Container</CardAction>
+            </CardHeader>
+            <CardContent>
+              <p>Status</p>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
