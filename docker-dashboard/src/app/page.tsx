1ba893b8c234d057330a5e688780c666a7423f0c
ArberAdemaj <arber.ademaj@students.fhnw.ch>
2026-01-12 13:45:10 +0100

    install nextjs

---
+++ docker-dashboard/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="flex justify-center pt-5 text-6xl">Docker Dashboard</h1>
+    </div>
+  );
+}
